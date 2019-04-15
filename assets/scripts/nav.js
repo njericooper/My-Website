457295d8e1cbcd72a4479a73ef20a70817d3c553
Njeri <njericooper@gmail.com>
2019-04-15 17:25:57 -0500

    added functioning nav and CSS

---
+++ assets/scripts/nav.js
@@ -0,0 +1,20 @@
+(function($) {
+    "use strict";
+  
+    var $navbar = $(".nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 0) {
+          $navbar.addClass("sticky");
+        } else {
+          $navbar.removeClass("sticky");  
+        }
+    });
+
+})(jQuery, undefined);
+
+$(".menu").click(function(){
+  $("#nav").toggleClass("open");
+});

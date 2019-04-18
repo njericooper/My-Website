c6e1a1cd1a4a980ad78d9f7c08285556856cf81f
Njeri <njericooper@gmail.com>
2019-04-18 15:43:47 -0500

    added single page scroll feature

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,20 +1,23 @@
+/*http://callmenick.com/post/expanding-search-bar-using-css-transitions*/
 (function($) {
-    "use strict";
-  
-    var $navbar = $(".nav"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 0) {
-          $navbar.addClass("sticky");
-        } else {
-          $navbar.removeClass("sticky");  
-        }
-    });
+  "use strict";
+
+  var $navbar = $(".nav"),
+      y_pos = $navbar.offset().top,
+      height = $navbar.height();
+
+  //scroll top 0 sticky
+  $(document).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop > 0) {
+        $navbar.addClass("sticky");
+      } else {
+        $navbar.removeClass("sticky");  
+      }
+  });
 
 })(jQuery, undefined);
 
 $(".menu").click(function(){
-  $("#nav").toggleClass("open");
+$("#nav").toggleClass("open");
 });

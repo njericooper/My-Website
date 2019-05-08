d0cb66ab18f9c535787b8810f2bc7f5007d60832
Njeri <njericooper@gmail.com>
2019-05-08 13:26:53 -0500

    added assets to master

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,27 @@
-const yo = {};
+(function($) {
+    "use strict";
+  
+    var $navbar = $(".nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+  
+    //scroll top 0 sticky
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 0) {
+          $navbar.addClass("sticky");
+        } else {
+          $navbar.removeClass("sticky");  
+        }
+    });
+  
+  })(jQuery, undefined);
+  
+  $(".menu").click(function(){
+  $("#nav").toggleClass("open");
+  });
+
+  const yo = {};
 
 yo.email_container = document.querySelector('.email_container');
 yo.form = document.querySelector('.email_container > #singular-form');

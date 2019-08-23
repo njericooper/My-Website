ff4118befd7414337663fd951000452c8424ec4a
Njeri <njericooper@gmail.com>
2019-08-23 16:09:05 -0500

    fixed footer

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,3 +1,4 @@
+
 var admin = require('firebase-admin');
 var refreshToken; // Get refresh token from OAuth2 flow
 
@@ -5,3 +6,5 @@ admin.initializeApp({
   credential: admin.credential.refreshToken(refreshToken),
   databaseURL: 'https://og-codes.firebaseio.com/'
 });
+
+

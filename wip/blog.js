c46018882a133e8b3d0e3cf377f9dd3843efb734
Njeri <njericooper@gmail.com>
2019-09-17 09:50:02 -0500

    blog feature complete

--- wip/blog.js
+++ wip/blog.js
@@ -1,14 +1,21 @@
-var myRequest = new XMLHttpRequest();
-myRequest.open('GET', 'https://api.github.com/users/njericooper/repos');
-myRequest.onreadystatechange = function () { 
-    // 4. check if the request has a readyState of 4, which indicates the server has responded (complete)
-    if (myRequest.readyState === 200) {
-        // 5. insert the text sent by the server into the HTML of the 'ajax-content'
-        document.getElementById('ajax-content').innerHTML = myRequest.responseText;
+function requestUserRepos(username){
+    const xhr = new XMLHttpRequest();
+    const url = `https://api.github.com/users/${username}/repos`;
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+        const data = JSON.parse(this.response);
+        for(let i in data) {
+            let ul = document.getElementById('userRepos');
+            let li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.innerHTML = (`
+            <p>Repo: ${data[i].name}</p>
+            <p>Description: ${data[i].description}</p>
+            <p>URL: <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+            `);
+            ul.appendChild(li);
+        }
     }
-};
-
-function sendTheAJAX() {
-    myRequest.send();
-    document.getElementById('reveal').style.display = 'none';
+    xhr.send();   
 }
+requestUserRepos('njericooper');

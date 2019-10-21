95c47ae8cae462646819b12c2b03884ad0b07e44
Njeri <njericooper@gmail.com>
2019-10-21 17:14:56 -0500

    fixed javascript inner html and updated css for footer text

--- public/assets/scripts/date.js
+++ public/assets/scripts/date.js
@@ -1,3 +1,8 @@
-var date = new Date();
-var theYear = date.getFullYear();
-document.getElementById("footerText").innerHTML = ' Coop ' + theYear;
+window.addEventListener('load', setDate, false);
+
+function setDate() { 
+    var date = new Date();
+    var theYear = date.getFullYear();
+    var theText = window.document.getElementById("footerText").innerHTML =  " &copy; " + " Coop " + theYear;
+    return theText;
+}
--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -6,4 +6,13 @@ admin.initializeApp({
   databaseURL: 'https://og-codes.firebaseio.com/'
 });
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDuWg8Y8lKT_ZnMdGbo2REGT_BrqR3tXW8",
+  authDomain: "og-codes.firebaseapp.com",
+  databaseURL: "https://og-codes.firebaseio.com",
+  projectId: "og-codes",
+  storageBucket: "og-codes.appspot.com",
+  messagingSenderId: "166622941576",
+  appId: "1:166622941576:web:43fae4fa738e1267"
+};
+firebaseConfig();

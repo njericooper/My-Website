8421f45e9379f783320a4171990cb8229f00ba12
Njeri <njericooper@gmail.com>
2019-10-25 16:57:20 -0500

    added api, html, and css to commerce estore

--- wip/eStore/store.js
+++ wip/eStore/store.js
@@ -1,5 +1,21 @@
 window.apicartConfig = window.apicartConfig || [];
-            function apicartConfigure(config){apicartConfig.push(config)}
-            apicartConfigure({
-                token: "<public api key>",
+function apicartConfigure(config){apicartConfig.push(config)}
+apicartConfigure({
+    token: "UjP71MPRsiXEvRt6K6etnDhevqMHVTaIKjSiHBXWxTiage6ChKi!U!riXzWm13nv",
+    currencySymbol: '$',
+    currencySymbolPositionLeft: true,
+    currencySymbolWithSpace: false,
+
+    init: function () {
+        Utils.loops.forEach(document.querySelectorAll('.buy-button'), function (key, button) {
+            apicart.cartQuantityManipulator.render({
+                el: button,
+                itemUrl: button.getAttribute('data-url'),
+                submitButton: 'Add To Cart'
             });
+        });
+    }
+
+});
+
+const data = JSON.parse(this.response);

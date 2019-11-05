c10c0cad565d1f7bf7baa1762cf56d95f593501f
Njeri <njericooper@gmail.com>
2019-11-05 15:59:03 -0600

    added SPA javascript native ad to test page

---
+++ wip/ads/ads.js
@@ -0,0 +1,11 @@
+amzn_assoc_tracking_id = "coopcodes-20";
+amzn_assoc_ad_mode = "manual";
+amzn_assoc_ad_type = "smart";
+amzn_assoc_marketplace = "amazon";
+amzn_assoc_region = "US";
+amzn_assoc_design = "enhanced_links";
+amzn_assoc_asins = "1617290750";
+amzn_assoc_placement = "adunit";
+amzn_assoc_linkid = "9712f48d94cb3013f4cf264ca2ef48cc"; 
+
+/*<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>*/

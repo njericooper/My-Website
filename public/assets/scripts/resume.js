1a609b03a697a63e13e11f873217231c4a5ca720
njericooper <njericooper@gmail.com>
2023-04-25 18:04:31 -0400

    styled resume Vue app

--- public/assets/scripts/resume.js
+++ public/assets/scripts/resume.js
@@ -1,13 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
+
             name: "Njeri Cooper" ,
             contact: "hello@njeri.codes | 931-797-7570",
-            currentJob: "Youth Villages",
-            currentDesc: "Development and User Interface design for organization program event and fundraising campaign pages. Maintaining website infrastructure using tools such as Digital Ocean Docker WP CLI child theme development PHP and JavaScript Managing tactical execution of email campaigns in Salesforce Marketing Cloud including developing HTML email templates, conducting QA reviews deployment  and analyzing delivery using validation and testing programs.",
-            prevJob1: "St Jude Childrens Research Hospital",
-            prevJob1Desc:"Filler Text",
-            skills: "list of skills",
+            profile: "Self-driven software engineer with seven years experience building products on mobile and web. Comfortable in fast-paced corporate, startup and nonprofit settings",
+            currentJob: "Web and WordPress Developer, Youth Villages - 2021-Present",
+            currentDesc: "Development and User Interface design for organization program event and fundraising campaign pages. Maintaining website infrastructure using tools such as Digital Ocean, Docker, WP CLI, WordPress theme development, PHP and JavaScript. Managing tactical execution of email campaigns in Salesforce Marketing Cloud including developing HTML email templates.",
+            freelance:"Freelance Software Developer (MERN / Serverless Stacks) - 2018-Present",
+            freelanceDesc:"Building quality web sites and web apps for clients in a timely manner utilizing WordPress, Gatsby, MERNStack",
+            prevJob1: "UI / UX / Mobile Developer, St. Jude - 2016-2018",
+            prevJob1Desc:"Led the planning, design, development, testing,and production deployment process for a new iteration for St.Jude's mobile app utilizing Apple's User Interface guidelines to set the tone for the design system and swift.The impact of this while launching it in partnership with Twitch totaled to $11 million raised for St.Jude.",
+            skills: "While I started my programming career being a polyglot programmer and solving any problem put in front of me I have since focused on the Javascript frameworks, cloud technologies, and efficent DevOps systems. My focus is on HTML, CSS, ES6, React, Vue, Front End Architecture ( SMACSS, BEM, Build Tools), Web Security, WordPress Development etc.",
             icon1: "https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80",
          /*
             icon1: 'https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80', */

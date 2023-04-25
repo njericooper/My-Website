2def53f36a1b638e3e0d9e4d865bf80fe7e70ca8
njericooper <njericooper@gmail.com>
2023-04-25 19:01:45 -0400

    resume app style update

--- public/assets/scripts/resume.js
+++ public/assets/scripts/resume.js
@@ -12,10 +12,12 @@ const app = Vue.createApp({
             prevJob1: "UI / UX / Mobile Developer, St. Jude | 2016-2018",
             prevJob1Desc:"Led the planning, design, development, testing,and production deployment process for a new iteration for St.Jude's mobile app utilizing Apple's User Interface guidelines to set the tone for the design system and swift.The impact of this while launching it in partnership with Twitch totaled to $11 million raised for St.Jude.",
             skills: "While I started my development career being a polyglot programmer, I have since focused on the Javascript frameworks, cloud technologies, and efficent DevOps systems. I'm most enthusiastic about solving problems using Front End Architecture (SMACSS, BEM, Build Tools), ES6, React, Vue, WordPress Development, Cloud Computing (Digital Ocean, AWS, Google), Web and Server Security, etc.",
-            icon1: "https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80",
+            icon1: "https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50",
          /*
             icon1: 'https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80', */
-            icon1Alt: ''
+            icon1Alt: "",
+            qrCode: "https://ucarecdn.com/3ce559f5-2718-4b38-af18-e2d653f27542/-/preview/586x270/-/quality/smart/-/format/auto/",
+            qrCodeAlt: ""
         }
     }
 })

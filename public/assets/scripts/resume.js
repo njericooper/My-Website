e93f2fad7612fcad7368410040b124437fe98d7f
njericooper <njericooper@gmail.com>
2023-04-25 15:30:24 -0400

    added resume page as Vue app

---
+++ public/assets/scripts/resume.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: "Njeri Cooper" ,
+            contact: "hello@njeri.codes | 931-797-7570",
+            currentJob: "Youth Villages",
+            currentDesc: "Development and User Interface design for organization program event and fundraising campaign pages. Maintaining website infrastructure using tools such as Digital Ocean Docker WP CLI child theme development PHP and JavaScript Managing tactical execution of email campaigns in Salesforce Marketing Cloud including developing HTML email templates, conducting QA reviews deployment  and analyzing delivery using validation and testing programs.",
+            prevJob1: "St Jude Childrens Research Hospital",
+            prevJob1Desc:"Filler Text",
+            skills: "list of skills",
+            icon1: "https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80",
+         /*
+            icon1: 'https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80', */
+            icon1Alt: ''
+        }
+    }
+})

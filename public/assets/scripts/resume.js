bd3a2efca84b2d4b218883b508abbc34449eb182
njericooper <njericooper@gmail.com>
2023-12-24 12:41:03 -0500

    updates

--- public/assets/scripts/resume.js
+++ public/assets/scripts/resume.js
@@ -4,14 +4,14 @@ const app = Vue.createApp({
 
             name: "Njeri Cooper" ,
             contact: "hello@njeri.codes | 931-797-7570",
-            profile: "Self-driven software engineer with seven years experience building products on mobile and web. Comfortable in fast-paced corporate, startup and nonprofit settings",
+            summary: "Self-driven software engineer. Focused on Javascript, Python, cloud technologies, and DevOps. I'm most enthusiastic about solving problems from a full stack perspective.",
             currentJob: "Web Developer, Youth Villages | 2021-Present",
             currentDesc: "Lead web developer on a local government-backed project that reduces gun violence and helps heal disenfranchised communities with therapy and job resources. Helped resolve multi-brand web accessibility issues by using APIs. This change directly decreased media storage and hosting costs by 50%",
             freelance:"Freelance Software Developer (Serverless) | 2018-Present",
             freelanceDesc:"Building quality web sites and web apps for clients in a timely manner utilizing WordPress, Gatsby, Eleventy, MERNStack",
             prevJob1: "UI / UX / Mobile Developer, St. Jude | 2016-2018",
             prevJob1Desc:"Led the planning, design, development, testing,and production deployment process for a new iteration for St.Jude's mobile app utilizing Apple's User Interface guidelines to set the tone for the design system and swift.The impact of this while launching it in partnership with Twitch totaled to $11 million raised for St.Jude.",
-            skills: "While I started my development career being a polyglot programmer, I have since focused on Javascript frameworks, cloud technologies, and efficent DevOps systems. I'm most enthusiastic about solving problems using Front End Architecture (SMACSS, BEM, Build Tools), ES6 and Human Interaction Design.",
+            skills: " (SMACSS, BEM, Build Tools), ES6 and Human Interaction Design.",
             icon1: "https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=50",
          /*
             icon1: 'https://images.unsplash.com/photo-1635776063043-ab23b4c226f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80', */
